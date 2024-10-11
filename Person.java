d9425130da299a512b9e753cfe719be5bb537c66
Trey <64178396+T-Dev-CCM@users.noreply.github.com>
2024-10-10 18:00:26 -0700

    Add files via upload

--- Person.java
+++ Person.java
@@ -20,9 +20,20 @@ public class Person {
 
     public static void main(String[] args){
 
-        Person person01 = new Person(first_name:"Droid", last_name: "32", age: "1");
-
-        obj_person01.introduce();
+        Person person01 = new Person(first_name:"Cody", last_name:"Rayworth", age:12);
+        obj_person01.introduce(); 
+        Person person02 = new Person(first_name:"Alice", last_name:"Carter", age:13);
+        obj_person02.introduce(); 
+        Person person03 = new Person(first_name:"Ash", last_name:"Hayes", age:26);
+        obj_person03.introduce(); 
+        Person person04 = new Person(first_name:"Isaac", last_name:"Cliffton", age:13); 
+        obj_person04.introduce(); 
+        Person person05 = new Person(first_name:"Droid", last_name:"32", age:1);
+        obj_person05.introduce();
+        
+        
+      
+  
     }
 
 }
